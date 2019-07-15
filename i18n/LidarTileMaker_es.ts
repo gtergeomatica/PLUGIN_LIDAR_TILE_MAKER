3e32fb39e96073d6e5e684c16eef507b0d23715b
RobiFag <robifagandini@hotmail.it>
2019-07-15 17:43:54 +0200

    Added spanish translation of the GUI

---
+++ i18n/LidarTileMaker_es.ts
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>LidarTileMaker</name>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="214"/>
+        <source>&amp;Lidar Tile Maker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="201"/>
+        <source>LIDAR tiles from DSM/DTM</source>
+        <translation>LIDAR tiles del DSM/DTM</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="442"/>
+        <source>PROCESS STARTED...
+</source>
+        <translation>PROCESO INICIADO...
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="447"/>
+        <source>ERROR: no DSM input folder has been selected
+</source>
+        <translation>ERROR: no se ha seleccionado una carpeta DSM de entrada
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="451"/>
+        <source>ERROR: no DTM input folder has been selected
+</source>
+        <translation>ERROR: no se ha seleccionado una carpeta DTM de entrada
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="455"/>
+        <source>ERROR: no CRS has been selected for input DSM and DTM
+</source>
+        <translation>ERROR: no se ha seleccionado un CRS para los DSM y DTM de entrada
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="459"/>
+        <source>WARNING: no CRS has been selected for the Vector Tile Layer. The DSM/DTM CRS will be used
+</source>
+        <translation>ADVERTENCIA: no se ha seleccionado un CRS para la capa de teselas vectoriales. Se usara el DSM/DTM CRS
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="465"/>
+        <source>WARNING: no Authority has been specified. The related field will be empty
+</source>
+        <translation>ADVERTENCIA: no se ha especificado ninguna autoridad. El campo relacionado estará vacío
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="469"/>
+        <source>WARNING: no Year has been specified. The related field will be empty
+</source>
+        <translation>ADVERTENCIA: no se ha especificado ningún año. El campo relacionado estará vacío
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="547"/>
+        <source>WARNING: Unable to open item named &apos;{}&apos;, it is not a file. Chek the input DTM folder
+</source>
+        <translation>ADVERTENCIA: No se puede abrir el elemento llamado &apos;{}&apos;, no es un archivo. Compruebe la carpeta de entrada DTM
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="550"/>
+        <source>ATTENTION! No DTM file have been found. Chek the input DTM folder
+</source>
+        <translation>ATENCIÓN! No se ha encontrado ningún archivo DTM. Compruebe la carpeta de entrada DTM
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="601"/>
+        <source>WARNING: Unable to open item named &apos;{}&apos;, it is not a file. Chek the input DSM folder
+</source>
+        <translation>ADVERTENCIA: No se puede abrir el elemento llamado &apos;{}&apos;, no es un archivo. Compruebe la carpeta de entrada DSM
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="605"/>
+        <source>ATTENTION! No DSM file have been found. Chek the input DSM folder
+</source>
+        <translation>ATENCIÓN! No se ha encontrado ningún archivo STM. Compruebe la carpeta de entrada DSM
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="667"/>
+        <source>NB: the number of DTM and DSM file is different, only the tile with both DTM and DSM will be created
+</source>
+        <translation>NB: el número de archivos DTM y DSM es diferente, solo se creará el tile con ambos DTM y DSM
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="683"/>
+        <source>ATTENTION! Impossible to create tiles polygons. Check DSM and DTM files, probably they have different extensions
+</source>
+        <translation>ATENCIÓN! Imposible crear tiles polygons. Revisa los archivos DSM y DTM, probablemente tengan diferentes extensiones
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="686"/>
+        <source>ATTENTION! There are {} DTM and {} DSM but only {} DTM/DSM with the same extention have been found. Check the raster file extentions
+</source>
+        <translation>ATENCIÓN! Hay {} DTM y {} DSM, pero solo se han encontrado {} DTM / DSM con la misma extensión. Compruebe las extensiones de archivo raster
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="702"/>
+        <source>ATTENTION! Impossible to create Tile Vector Layer, the input DTM folder is empty
+</source>
+        <translation>ATENCIÓN! Imposible crear Tile Vector Layer, la carpeta DTM de entrada está vacía
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="705"/>
+        <source>ATTENTION! Impossible to create Tile Vector Layer, the input DSM folder is empty
+</source>
+        <translation>ATENCIÓN! Imposible crear Tile Vector Layer, la carpeta DSM de entrada está vacía
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="708"/>
+        <source>ATTENTION! Impossible to create Tile Vector Layer, the input DSM and DTM folders are empty
+</source>
+        <translation>ATENCIÓN! Imposible crear Tile Vector Layer, las carpetas de entrada DSM y DTM están vacías
+</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker.py" line="711"/>
+        <source>PROCESS FINISHED...
+</source>
+        <translation>PROCESO TERMINADO...
+</translation>
+    </message>
+</context>
+<context>
+    <name>LidarTileMakerDialogBase</name>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="14"/>
+        <source>Lidar Tile Maker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="22"/>
+        <source>INPUT data and parameters</source>
+        <translation>Datos y parámetros de ENTRADA</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="34"/>
+        <source>Input DSM folder</source>
+        <translation>Carpeta de entrada DSM</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="173"/>
+        <source>Browse..</source>
+        <translation>Examinar..</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="55"/>
+        <source>Input DTM folder</source>
+        <translation>Carpeta de entrada DTM</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="80"/>
+        <source>DSM/DTM CRS</source>
+        <translation>DSM/DTM CRS</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="91"/>
+        <source>Authority</source>
+        <translation>Autoridad</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="117"/>
+        <source>Year</source>
+        <translation>Año</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="151"/>
+        <source>OUTPUT</source>
+        <translation>SALIDA</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="159"/>
+        <source>Tile Output folder</source>
+        <translation type="unfinished">Carpeta de salida Tile</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="180"/>
+        <source>Vector tile CRS</source>
+        <translation>Vector tile CRS</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="197"/>
+        <source>Clear Log</source>
+        <translation>Borrar registro</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="204"/>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../lidar_tile_maker_dialog_base.ui" line="224"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
